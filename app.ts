6718c408d8c1885c2303983195d2f065fce90413
Hector <hlu0723@student.skelleftea.se>
2025-10-08 09:44:43 +0200

    datatyper och arayer påbörjad

---
+++ app.ts
@@ -0,0 +1,12 @@
+/*
+let f_namn = await read("Förnamn? ")
+let e_namn = await read("Efternamn? ")
+
+write(
+    "Dina initialer är:"+
+    f_namn.substring(0,1)+
+    e_namn.substring(0,1)
+);
+*/
+
+export { }
