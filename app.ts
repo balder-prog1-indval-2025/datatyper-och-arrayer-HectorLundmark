c0bfe1513d33eeb61233bbd059216abfe25504f6
Hector <hlu0723@student.skelleftea.se>
2025-10-15 09:41:27 +0200

    Datatyper och arrayer påbörjad

--- app.ts
+++ app.ts
@@ -33,5 +33,32 @@ let b= +await read("B: ")
 let c= +await read("C: ")
 write("Längsta diagonalen: " + Math.sqrt(a**2+b**2+c**2))
 */
+/*
+let primtal=[2, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+write("Första primtalet: " + primtal[(0)]);
+write("Åttonde primtalet: " + primtal[(7)]);
+write("Längd (Utan nionde primtalet): " + primtal.length);
+primtal.push(23);
+write("Längd med alla primtal: " + primtal.length);
+*/
+/*
+let städer = [
+    "Rom",
+    "Paris",
+    "Berlin",
+    "Stockholm",
+    "Moskva",
+];
+let länder = [
+    "Italien",
+    "Frankrike",
+    "Tyskland",
+    "Sverige",
+    "Ryssland",
+];
+
+for(let i=0, i < , i++);
+    write(städer[i] + "är huvudstad i"+ länder[i]);
+    */
 
 export { }
